e47e214c05e81f88047ee49feecd7f5d3d9da8ad
Rody Campos <rodycamposgo@singularities.com>
2016-10-09 14:27:01 -0600

    Add files to master repository

---
+++ src/main/java/cr/ac/una/globales/DreamerApplication.java
@@ -0,0 +1,12 @@
+package cr.ac.una.globales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamerApplication.class, args);
+	}
+}
