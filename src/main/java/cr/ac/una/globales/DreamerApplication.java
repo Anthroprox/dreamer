91615d2624f9d41dc8990e8840a5b599d768a4a5
Rody Campos <rodycamposgo@gmail.com>
2016-10-09 15:24:51 -0600

    test

--- src/main/java/cr/ac/una/globales/DreamerApplication.java
+++ src/main/java/cr/ac/una/globales/DreamerApplication.java
@@ -2,9 +2,19 @@ package cr.ac.una.globales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DreamerApplication {
+    
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DreamerApplication.class, args);
